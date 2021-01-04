b3c19a064200406ec2c18574f86e6045e811f5d9
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-04 18:34:14 +0100

    feat: add convertDataToTensor method

--- src/app.service.ts
+++ src/app.service.ts
@@ -37,4 +37,37 @@ export class AppService {
 
     return model;
   }
+
+  convertDataToTensor(data) {
+    return tf.tidy(() => {
+      // 1. Shuffle the data
+      tf.util.shuffle(data);
+
+      // 2. Convert data to Tensor
+      const inputs = data.map(d => d.horsepower);
+      const labels = data.map(d => d.mpg);
+
+      const inputTensor = tf.tensor2d(inputs, [inputs.length, 1]);
+      const labelTensor = tf.tensor2d(labels, [labels.length, 1]);
+
+      // 3. Normalize the data to the range 0-1
+      const inputMax = inputTensor.max();
+      const inputMin = inputTensor.min();
+      const labelMax = labelTensor.max();
+      const labelMin = labelTensor.min();
+
+      const normalizedInputs = inputTensor.sub(inputMin).div(inputMax.sub(inputMin));
+      const normalizedLabels = labelTensor.sub(labelMin).sub(labelMax.sub(labelMin));
+
+      // 4. Return all Tensor data
+      return {
+        inputs: normalizedInputs,
+        labels: normalizedLabels,
+        inputMax,
+        inputMin,
+        labelMax,
+        labelMin,
+      }
+    });
+  }
 }
