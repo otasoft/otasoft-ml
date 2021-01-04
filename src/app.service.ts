636e8e204825df9a16c3bc73cf85d274a0fcb72a
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-04 19:30:48 +0100

    feat: add testModel method

--- src/app.service.ts
+++ src/app.service.ts
@@ -88,4 +88,42 @@ export class AppService {
       shuffle: true,
     })
   }
+
+  testModel(model, inputData, normalizationData) {
+    const {inputMax, inputMin, labelMin, labelMax} = normalizationData;  
+  
+    // Generate predictions for a uniform range of numbers between 0 and 1;
+    // We un-normalize the data by doing the inverse of the min-max scaling 
+    // that we did earlier.
+    const [xs, preds] = tf.tidy(() => {
+      
+      const xs = tf.linspace(0, 1, 100);      
+      const preds = model.predict(xs.reshape([100, 1]));      
+      
+      const unNormXs = xs
+        .mul(inputMax.sub(inputMin))
+        .add(inputMin);
+      
+      const unNormPreds = preds
+        .mul(labelMax.sub(labelMin))
+        .add(labelMin);
+      
+      // Un-normalize the data
+      return [unNormXs.dataSync(), unNormPreds.dataSync()];
+    });
+
+
+    const predictedPoints = Array.from(xs).map((val, i) => {
+      return {x: val, y: preds[i]}
+    });
+    
+    const originalPoints = inputData.map(d => ({
+      x: d.horsepower, y: d.mpg,
+    }));
+
+    return {
+      predictedPoints,
+      originalPoints,
+    }
+  }
 }
