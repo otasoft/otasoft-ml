7979a43cda25209e4649037a7818e3fd89905bbc
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-16 16:43:02 +0100

    feat-#2: correctly serve Angular frontend

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,7 +7,7 @@ import { AppService } from './app.service';
 @Module({
   imports: [
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'client')
+      rootPath: join(__dirname,'../..', 'client/dist/client')
     }),
     HttpModule,
   ],
