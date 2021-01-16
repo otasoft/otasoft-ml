b6ab9dd72a4ead7188222db74bfdbd3567270f50
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-16 17:29:28 +0100

    feat-#2: add tensorflow visual representation of a linear regression

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
-import * as tf from '@tensorflow/tfjs';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import * as tfvis from '@tensorflow/tfjs-vis';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'client';
+
+  constructor(private http: HttpClient) {}
+
+  async ngOnInit() {
+    const response: any = await this.http.get('/api/tensorflow-test').toPromise()
+
+    tfvis.render.scatterplot(
+      { name: 'Tensorflow test' },
+      { values: [response.originalPoints, response.predictedPoints], series: ['original', 'predicted'] },
+      {
+        xLabel: 'Horsepower',
+        yLabel: 'MPG',
+        height: 300
+      }
+    )
+  }
 }
