d3a4c86572f312e3bc37198059a045d19d17e193
Baroshem <jakub.andrzejewski.dev@gmail.com>
2021-01-16 16:20:54 +0100

    feat-#2: add serve static module to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,16 @@
 import { HttpModule, Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [HttpModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client')
+    }),
+    HttpModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
